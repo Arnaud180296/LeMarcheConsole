bbd5812f1ca42a322cd3e3ce0e44240e44c1316a
Arnaud180296 <1802arkabimbingoy@gmail.com>
2025-09-23 00:18:44 +0200

    Implémentation des méthodes GoBackMainMenu(), IsFruitBasketEmpty() et Refactorisation de la methode DisplayFruitBasket()

--- LeMarcheConsole/Program.cs
+++ LeMarcheConsole/Program.cs
@@ -5,9 +5,11 @@
         ///<summary>
         /// Ajoute un element de type string à un tableau de type string.
         ///</summary>
-        ///<param name="fruitBasket"> Tableau à une dimension permettant le stockage de chaine de caractères</param>
-        ///<returns>Renvoie ...</returns>
-        
+        ///<param name="fruitBasket"> Tableau à une dimension de chaine de caracteres permettant le stockage de fruits</param>
+        ///<param name="fruit"> Chaine de caractères représentant un fruit à ajouter au panier</param>
+        ///<param name="BASKETFRUITLIMIT"> Constante de type entier definissant la taille maximale du panier</param>
+        ///<returns>Renvoie un tableau de string, servant à représenter le panier à fruit, après ajout d'un nouvel élément</returns>
+
         if (fruitBasket.Length > BASKETFRUITLIMIT - 1)
         {
             Console.BackgroundColor = ConsoleColor.Red;
@@ -24,7 +26,6 @@
         }
 
         //===============CopyOldTabValuesToNewTab=========================
-        
         //Ceci doit etre factoriser dans la methode CopyOldTabValuesToNewTab(fruitBasket);
         string[] newFruitBasket = new string[fruitBasket.Length + 1];
         //Console.WriteLine(newFruitBasket.Length);
@@ -37,8 +38,11 @@
             }
         }
         newFruitBasket[fruitBasket.Length] = fruit;
+        Console.Beep(2100, 200);
+        Console.Beep(2100, 200);
         //=====================
-
+        Console.ResetColor();
+        Console.Clear();
         return newFruitBasket;
     }
 
@@ -49,26 +53,54 @@
     }
     public static void DisplayFruitBasket(string[] fruitBasket)
     {
-        if (fruitBasket.Length == 0)
-        {
-            Console.BackgroundColor = ConsoleColor.Red;
-            Console.WriteLine("Aucun fruit trouvé dans le panier...");
-            Console.BackgroundColor = ConsoleColor.Black;
+        if (IsFruitBasketEmpty(fruitBasket))
             return;
-        }
+
         Console.WriteLine("Voici le contenu du panier : ");
-        foreach (string fruit in fruitBasket)
-            Console.WriteLine($"\t- {fruit}");
+        Foo(fruitBasket);
+        
+        Console.ResetColor();
+
+        GoBackMainMenu();
+
+        Console.Clear();
+        return;
+    }
+    
+    public static void Foo(string[] elements) //TODO : Trouver un nom de methode plus adequat
+    {
+        int i = 0;
+        foreach (string element in elements)
+        {
+            if (i % 2 == 0)
+            {
+                Console.BackgroundColor = ConsoleColor.DarkGray;
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Gray;
+                Console.ForegroundColor = ConsoleColor.Black;
+            }
+            Console.WriteLine($"\t+ {element}");
+            i++;
+        }
     }
     public static void DisplayMenu(string[] menuOptions)
     {
-        foreach (string option in menuOptions)
-        {
-            Console.WriteLine($"* {option}");
-        }
-        Console.WriteLine("");
+        Console.WriteLine("[_Options disponible_]");
+        Foo(menuOptions);
+        Console.ResetColor();
     }
 
+    public static void FindFruitBasket(string[] fruitBasket, params string[] fruitsToFind)
+    {
+        //TODO faire une surcharge de methode pour que si plus de 1 fruit est recherché
+        //la surcharge est appelée. Pour gerer les parametres multiple UTILISER ...nomParam
+        foreach (var fruit in fruitsToFind)
+        {
+            
+        }
+    }
     public static void FindFruitBasket(string[] fruitBasket, string fruitToFind = "fruitTest")
     {
         foreach (string fruit in fruitBasket)
@@ -76,17 +108,30 @@
             if (fruit == fruitToFind)
             {
                 Console.WriteLine($"{fruitToFind} a déjà été acheté");
+                GoBackMainMenu();
                 return;
             }
         }
         Console.WriteLine($"\"{fruitToFind}\" n'existe pas dans le panier");
+
+        GoBackMainMenu();
+
         return;
     }
-    /*
-     * Mise à jour de InputFruit() et de la saisie choice =>
-        Prise en charge du warning CS8602 : Déréférencement d'une éventuelle référence null.*
-     
-     */
+
+    public static void GoBackMainMenu()
+    {
+        do
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Appuyer sur la touche \"{ConsoleKey.Enter}\" du clavier pour retourner au menu...");
+        } while ((Console.ReadKey().Key != ConsoleKey.Enter));
+
+        Console.Clear();
+        
+        return;
+    }
+
     public static string InputFruit()
     {
         Console.Write("Saisir le nom du fruit en question : ");
@@ -100,19 +145,42 @@
 
     public static bool IsDuplicate(string[] fruitBasket, string fruit)
     {
+        //TODO : ...
         return false;
     }
 
-    public static string[] RemoveFruit(string[] fruitBasket, string fruit)
+    public static bool IsFruitBasketEmpty(string[] fruitBasket)
     {
-        //controller le fait de ne pas pouvoir supprimer un fruit qui n'est pas dans le panier
-        // controller le fait de ne pas pouvoir descendre en dessous de 0 element dans le panier
-        Console.WriteLine($"\"{fruit}\" a été retiré du panier");
+        if (fruitBasket.Length == 0)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("Aucun fruit trouvé dans le panier...");
+            Console.BackgroundColor = ConsoleColor.Black;
+            return true;
+        }
+        return false;
+    }
+
+    public static string[] RemoveFruit(string[] fruitBasket, string fruitToRemove)
+    {
+        if (fruitBasket.Length < 1)
+            return fruitBasket;
+
+        foreach (string fruit in fruitBasket)
+        {
+            if (fruit == fruitToRemove)
+            {
+                //etape de suppression
+                Console.WriteLine($"\"{fruitToRemove}\" a été retiré du panier");
+            }
+        }
+     
         return fruitBasket;
     }
 
     public static void Main(string[] args)
     {
+        Console.Title = "Jeu du marché";
         bool loop = true;
         string[] menuOptions = ["[A]fficher", "[Aj]outer", "[R]echercher", "[S]upprimer", "[Q]uitter"];
         const int BASKETFRUITLIMIT = 5;
@@ -120,12 +188,15 @@
         string? choice;
         string fruit;
 
+        //LIRE CECI POUR INTEGRER LA GESTION DU CLAVIER
+        //https://learn.microsoft.com/fr-fr/dotnet/api/system.consolekey?view=net-8.0
+        //https://learn.microsoft.com/fr-fr/dotnet/api/system.consolekeyinfo?view=net-8.0
+        //https://learn.microsoft.com/fr-fr/dotnet/api/system.consolekeyinfo?view=net-8.0
 
         while (loop)
         {
-            Console.ForegroundColor = ConsoleColor.White;
+            Console.ResetColor();
             
-            Console.WriteLine("[...Jeu du marché...]");
             DisplayMenu(menuOptions);
 
             Console.Write("Saisir l'option souhaitée : ");
@@ -141,7 +212,7 @@
                     break;
 
                 case "ajouter" or "aj":
-                    Console.Clear();
+                    //Console.Clear();
                     Console.ForegroundColor = ConsoleColor.Green;
                     fruit = InputFruit();
                     fruitBasket = AddFruit(fruitBasket, fruit, BASKETFRUITLIMIT);
@@ -171,7 +242,6 @@
                     Console.WriteLine($"La commande : \"{choice}\" n'est pas implémenté...");
                     break;
             }
-            
         }
         Console.WriteLine("Fin du programme...");
         return;
